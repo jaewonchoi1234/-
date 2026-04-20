b69acd42fe4b82be4a3a6a4a8417afe31612fd15
qwaszx9743@naver.com <qwaszx9743@naver.com>
2026-04-20 16:59:46 +0900

    ssh 접속 수정

--- script.py
+++ script.py
@@ -1,15 +1,14 @@
-from datetime import datetime
-import os
 import asyncssh
 import pandas as pd
 import asyncio
-import sys
+import os
+
 
 
 def get_command_from_txt(path):
     commands=[]
     with open(path,'r',encoding="utf-8") as f:
-        lines = f.readlines
+        lines = f.readlines()
         for line in lines:
             commands.append(line.strip())
     return commands
@@ -23,98 +22,68 @@ def get_session_from_csv(filename):
 
 async def run_client(host,commands):
     result = []
-    async with asyncssh.connect(host) as conn:
-        for command in commands:
-            result.append(await conn.run(command))
+    async with sem:
+        try:
+            async with asyncssh.connect(host,username="tdcv123",password="MISgortla~09") as conn:
+                for command in commands:
+                    try:
+                        r=await conn.run(command)
+                        result.append({'command':command,'result':r.stdout, 'success': True})
+                    except Exception as e:
+                        result.append({'command':command,'result':str(e), 'success': False, 'host': host})
+        except Exception as e:
+            result.append({'result':str(e), 'success': False, 'host': host})
         return result
 
 
 async def run_clients(session,commands):
-    results = []
-    for host in session:
-       try:
-            asyncio.get_event_loop().run_until_complete(run_client())
-       except (OSError, asyncssh.Error) as exc:
-            sys.exit('SSH connection failed: ' + str(exc))
-       await results.append(run_client(host,commands))
-    return results
+    tasks = [run_client(host, commands) for host in session]
+    result = await asyncio.gather(*tasks)
+    success_list = [r for r in result if r['success'] == True]
+    fail_list = [r for r in result if r['success'] == False]
+    return success_list, fail_list
 
 
-def parse_result(result):
-    result.stdout
+def make_csv_session(session, file_name):
+    data = {'host': session}
+    df = pd.DataFrame(data)
+    os.makedirs("csv", exist_ok=True)
+    if os.path.exists("csv/"+file_name):
+        raise FileExistsError(f"{file_name}은 이미 존재합니다.")
+    df.to_csv('csv/'+file_name+'.csv')
 
 
-def make_logfile():
+#def parse_result(results, lenth):
+ #   #for result in results:
+  #      for i in range(0,lenth-1):
+   #         print(results[i])
 
 
-
-def check_hardware():
-
-
-
-def print_log():
-
-
-
-def compare_config():
+#def make_logfile():
 
 
 
-def make_report():
+#def check_hardware():
 
 
 
+#def print_log():
 
 
-def remoteInspection(session):
-    #tatadaewoo
-    username = 'tdcv123'
-    password = ''
+
+#def compare_config():
 
 
-    today = datetime.today().strftime('%Y%m%d')
-    os.mkdir('./logs/' + today)
-    #세션 접속
-    for session in session:
-        if session['protocol'] == 'SSH2':
 
-            #ssh 접속
-            client = paramiko.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect(session['host'],session['port'],username,password)
+#def make_report():
 
-            # sh run에서 banner 찾아서 출력값에서 banner 뺴기
-            stdin, stdout, stderr = client.exec_command('show run')
-            text = stdout.read().decode('utf-8')
-            banner_index = text.find('banner motd ^C')+15
-            banner = text[banner_index:].split('^C')[0].strip()
-            no_banner = text[text.find(banner)+len(banner):]
-
-            # hostname 찾기
-            hostname_index=no_banner.find('hostname')+9
-            if hostname_index!=8:
-             hostname = no_banner[hostname_index:].split('\n')[0].strip()
-            else:
-             hostname = 'unknown'
-
-            # 파일 생성
-            file_name= session['host']+hostname+datetime.now().strftime("_%Y-%m-%d_%H:%M:%S")
-            file = open("./logs/"+today+'/'+file_name,'w')
-
-            # ssh 명령어 입력 및 결과값 파일에 쓰기
-            
-            for command in command:
-                stdin, stdout, stderr = client.exec_command(command)
-                text = stdout.read().decode('utf-8')
-                file.write(hostname+'#'+command+'\n')
-                file.write(text)
-        elif session['protocol'] == 'Telnet':
-            =telnetlib3.open_connection(session['host'],session['port'])
 
 
 if __name__ == '__main__':
-    commands = get_command_from_txt('command')
-    session = get_session_from_csv('csv/tata_csv')
-    results = run_clients(session,commands)
-
+    commands = get_command_from_txt('commands/command.txt')
+    session = get_session_from_csv('csv/tata_csv.csv')
+    sem = asyncio.Semaphore(25)
+    success_list , fail_list = run_clients(session,commands)
+    make_csv_session([fail['host'] for fail in fail_list], 'csv/tata_fail.csv')
+   # parse_result(results,len(commands))
 
